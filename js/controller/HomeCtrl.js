084645aa0b4ddc8468b3833f9b7f6a1e328558fb
jchul <jchul3396@gmail.com>
2016-10-29 15:57:01 +0700

    Typo fix

--- js/controller/HomeCtrl.js
+++
@@ -1,9 +0,0 @@
-myapp.controller("HomeCtrl", function ($scope) {
-
-	$("#loginButton").click(function () {
-		$.get("u/" + $("#loginTextbox").val(), function (data) {
-			console.log(data);
-		});
-	});
-
-})
