7824c6daa16722391ad97711e4ef7bd95d37af64
jchul <jchul3396@gmail.com>
2016-10-29 15:51:08 +0700

    Factory setup

---
+++ js/PlayCtrl.js
@@ -0,0 +1,33 @@
+/*
+##	This is the controller for the game page.
+ */
+
+myapp.controller('PlayCtrl', function ($scope) {
+
+	$scope.num = [1, 2, 3, 4, 5];
+	$scope.ans = 'some num';
+
+	var myName;
+	var opponentName;
+	var myNumber;
+	var roomNumber;
+
+	//CHAT AND LOG
+
+	$('form').submit(function () {
+		socket.emit('chat message', {
+			'roomNumber' : roomNumber,
+			'msg' : $('#m').val()
+		});
+		$('#m').val('');
+		return false;
+	});
+
+	socket.on('chat message', function (msg) {
+		$('#messages').append($('<li>').text(msg));
+	});
+	socket.on('log', function (msg) {
+		console.log(msg);
+	});
+	*/
+})
