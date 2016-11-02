fe0d97f141261ddaa6c46111988765a0ac693513
Kaneks <kanek.eks@gmail.com>
2016-11-02 21:46:07 +0700

    also this

--- js/PlayCtrl.js
+++ js/PlayCtrl.js
@@ -2,10 +2,42 @@
 ##	This is the controller for the game page.
  */
 
-myapp.controller('PlayCtrl', function ($scope,$location, socketio) {
+myapp.controller("PlayCtrl", ['$rootScope','$scope','$location','socketio',function ($rootScope,$scope, $location, socketio) {
 
-	$scope.num = [1, 2, 3, 4, 5];
-	$scope.ans = 'some num';
+	var ans = null;
+	var probNums = null;
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "http://localhost:3000/question",
+		"method": "GET",
+		"headers": {
+			"cache-control": "no-cache",
+			"postman-token": "1acc73f6-96cc-809e-aebd-d6b2726566d6"
+		}
+	}
+
+	$.ajax(settings).done(function (response) {
+		$scope.num = [response.Num1, response.Num2, response.Num3, response.Num4, response.Num5];
+		$scope.ans = response.Ans;
+		ans = response.Ans;
+		probNums = [response.Num1, response.Num2, response.Num3, response.Num4, response.Num5];
+	});
+
+	$scope.checkAns = function(){
+        var ansFormula = $scope.ansField;
+		var ansNums = ansFormula.match(/\d/g).map(Number);
+
+		if(angular.equals(ansNums.sort(),probNums.sort())){
+			if(eval(ansFormula) == ans){
+				console.log('RIGHT');
+			}else{
+				console.log('WRONG');
+			}
+		}else{
+			console.log('INCOMPLETE');
+		}
+	};
 	//CHAT AND LOG
 
 	/*
